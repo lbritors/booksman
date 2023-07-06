c2bb536f39fab2e3d4b27c8582f694c2ff471635
Letícia Brito <90286379+lbritors@users.noreply.github.com>
2023-07-06 18:15:42 -0300

    Update books-repository.ts

--- src/repositories/books-repository.ts
+++ src/repositories/books-repository.ts
@@ -27,7 +27,7 @@ export async function reviewBook(bookReview: CreateReview) {
   return await prisma.books.update({
     data: bookReview,
     where: { 
-      bookId
+      id: bookId
     }
   });
 }
